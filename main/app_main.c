7c3c36093aff34ac7f9ff5aa1dceb666548b8a8f
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2025-11-20 22:37:51 -0300

    chore: initial project file and dependencies

---
+++ main/app_main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "esp_log.h"
+#include "sdkconfig.h"
+
+#include "app.h"
+#include "mq.h"
+#include "digital_io.h"
+
+static const char *TAG = "app_main";
+
+/** @brief App entry point */
+void app_main(void) {
+
+    ESP_ERROR_CHECK(app_init());
+    ESP_LOGI(TAG, "FreeRTOS resources created");
+
+    ESP_ERROR_CHECK(digital_io_task_init());
+    ESP_LOGI(TAG, "Digital IO peripherals initialized");
+
+    ESP_ERROR_CHECK(mq_task_init());
+    ESP_LOGI(TAG, "Analog inputs for MQs initialized");
+
+    ESP_LOGI(TAG, "Deleting main task");
+    vTaskDelete(NULL);
+}
